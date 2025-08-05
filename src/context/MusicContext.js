4aae9d39200dba0ac70d7ae21ae20d2bd93fbdd5
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2025-08-05 15:43:07 +0530

    Update MusicContext.js

--- src/context/MusicContext.js
+++ src/context/MusicContext.js
@@ -1,33 +1,3 @@
-import { createContext, useState, useEffect, useRef } from "react";
-import { getSongById } from "../../fetch";
-
+import { createContext } from "react";
 const MusicContext = createContext();
-
-export const MusicProvider = ({ children }) => {
-  const [currentSong, setCurrentSong] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [shuffle, setShuffle] = useState(false);
-  const [repeatMode, setRepeatMode] = useState("none"); // "none", "one", "all"
-  const [song, setSong] = useState(null);
-  const audioRef = useRef(new Audio());
-
-  useEffect(() => {
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.pause();
-        audioRef.current = null;
-      }
-    };
-  }, []);
-
-  const playMusic = async (songId) => {
-    try {
-      const response = await getSongById(songId);
-      const songData = response.data[0];
-
-      if (!songData || !songData.downloadUrl) {
-        console.error("Song data or download URL is missing.");
-        return;
-      }
-      
-      const qualities = songData.downloadUrl.map(item
+export default MusicContext; 
